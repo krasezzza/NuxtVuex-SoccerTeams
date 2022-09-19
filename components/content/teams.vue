3a34e794e18583eb380460fd4aa7b47d4a666c85
Krasimir Tsvetanov <krasimir.tsvetanov@whitelotusorder.com>
2022-09-19 17:52:14 +0300

    implement followed teams listing

--- components/content/teams.vue
+++ components/content/teams.vue
@@ -2,8 +2,46 @@
   <div class="section-teams">
     <h4>MY TEAMS</h4>
 
-    <div class="no-teams">
+    <ul
+      v-if="followedTeams.length"
+      class="followed-list"
+    >
+      <li
+        v-for="team in followedTeams"
+        :key="team.id"
+      >
+        <div class="followed-list-item">
+          <div class="team-placeholder">
+            <img
+              :src="require('~/assets/img/team.png')"
+              alt="team placeholder"
+            >
+          </div>
+
+          <div class="team-details">
+            <p class="team-name">
+              {{ team.name }}
+            </p>
+          </div>
+        </div>
+      </li>
+    </ul>
+
+    <div
+      v-else
+      class="no-teams"
+    >
       <span>You aren't following any teams yet.</span>
     </div>
   </div>
 </template>
+
+<script>
+  export default {
+    computed: {
+      followedTeams() {
+        return []
+      }
+    }
+  }
+</script>
